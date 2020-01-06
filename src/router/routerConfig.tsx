bcf7c9d3f232f2f23c42733a2b1dc40772b145b9
hammer <hammergwx@163.com>
2020-01-06 14:28:09 +0800

    add login2.0

--- src/router/routerConfig.tsx
+++ src/router/routerConfig.tsx
@@ -29,6 +29,9 @@ export default {
         }, {
             path: '/main/my',
             component: () => <My />
+        },{
+            from: '/main',
+            to: '/main/home'
         }]
     }, {
         from: '*',
