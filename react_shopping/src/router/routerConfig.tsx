2711130154cb88d9d5f2d87fb6f0793d93c41704
hammer <hammergwx@163.com>
2020-01-06 11:20:08 +0800

    add login

---
+++ react_shopping/src/router/routerConfig.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import RouterView from './RouterView'
+import Index from "../view/Index"
+import Home from "../view/Index/Home"
+import Classify from "../view/Index/Classify"
+import Cart from "../view/Index/Cart"
+import My from "../view/Index/My"
+import Login from "../view/Login"
+
+export default {
+    routes: [{
+        path: '/login',
+        component: () => <Login />
+    }, {
+        path: '/main',
+        component: (props: any) => (<>
+            <Index />
+            <RouterView routes={props.routes}></RouterView>
+        </>),
+        children: [{
+            path: '/main/home',
+            component: () => <Home />
+        }, {
+            path: '/main/classify',
+            component: () => <Classify />
+        }, {
+            path: '/main/cart',
+            component: () => <Cart />
+        }, {
+            path: '/main/my',
+            component: () => <My />
+        }]
+    }, {
+        from: '*',
+        to: '/login'
+    }]
+}
---
+++ react_shopping/src/view/Index/Classify/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Classify:React.FC = () => {
+    return (
+        <div>
+            <p>分类</p>
+        </div>
+    );
+};
+
+export default Classify;
