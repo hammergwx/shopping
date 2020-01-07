1e0d2a6207e33cc8dba51a9e247d9208c7df407c
MisakiChen125 <1056832687@qq.com>
2020-01-07 11:31:49 +0800

    request add

--- src/view/Index/Classify/index.tsx
+++ src/view/Index/Classify/index.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
-import './classify.css'
+import React,{useEffect} from 'react';
+import useStore from '../../../utils/useStore'
+import './classify.css';
+import {useObserver} from 'mobx-react-lite'
 const Classify:React.FC = () => {
-    return (
+    let store=useStore();
+    let {Classift} =store;
+    useEffect(()=>{
+        Classift.coctlogIndex()
+    },[])
+    return useObserver(()=>(
       
             <div className="classify">
-                <div className="box"></div>
+                <div className="serachbox">
+                    <input type="text" placeholder='搜索商品'/>
+                </div>
+                <div className="classify-content">
+                    <div className="c-left">
+                        {
+                            Classift.list.map((item,index)=>{
+                                return <div className='barItem'  key={index}>{item.name}</div>
+                            })
+                        }
+                    </div>
+                    <div className="c-right"></div>
+                </div>
             </div>
        
-    );
+    ));
 };
 
 export default Classify;
