4fcf74f60d34f3e1f087686eeabe443fd64b6cc5
2234837386 <2234837386@qq.com>
2020-01-10 14:06:19 +0800

    困死

---
+++ src/view/Detail/subDetail/sbuDetail.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../../../iconfont/iconfont.css'
+
+export default ()=>{
+    return (
+        <div className='noTabPageContent'>
+            <div className='topicDetail'>
+                <div className='left'>
+                    <i className='iconfont icon-i-left'></i>
+                </div>
+                <div className='title'>关爱他成长的每一个足迹</div>
+            </div>
+        </div>
+    )
+}
